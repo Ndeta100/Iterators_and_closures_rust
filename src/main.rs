b064c34cb738b657c98ab576fcfa1d46f5525e14
Ndeta Innocent <you@example.com>
2022-08-04 18:36:44 +0300

    using closures and Fn trait

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,52 @@ fn main() {
     let simulated_random_number = 7;
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
-fn simulated_expensive_calculatioin(intensity: u32) -> u32 {
-    println!("calculating slowly..");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
 }
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_closure = Cacher::new(|num: u32| {
+        println!("calculating slowly..");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
-        println!(
-            "Today do {} pushups",
-            simulated_expensive_calculatioin(intensity)
-        );
-        println!(
-            "Next, do {}  stiups",
-            simulated_expensive_calculatioin(intensity)
-        );
+        println!("Today do {} pushups", expensive_closure.value(intensity));
+        println!("Next, do {}  situps", expensive_closure.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated");
         } else {
             println!(
-                "TOday, run for {} minutes",
-                simulated_expensive_calculatioin(random_number)
+                "Today, run for {} minutes",
+                expensive_closure.value(intensity)
             );
         }
     }
